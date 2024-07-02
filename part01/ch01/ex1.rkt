033015fa92758b48a28d66c1fe96e7bdd18cc305
Hung <hungcs192@gmail.com>
2024-07-02 14:41:47 +0700

    Excercise in ch1/p1

---
+++ part01/ch01/ex1.rkt
@@ -0,0 +1,6 @@
+#lang htdp/bsl
+
+(define x 3)
+(define y 4)
+
+(sqrt (+ (expt x 2) (expt y 2)))
---
+++ part01/ch01/ex2.rkt
@@ -0,0 +1,7 @@
+#lang htdp/bsl
+(require 2htdp/image)
+
+(define p "hello")
+(define s "world")
+
+(string-append p "_" s)
